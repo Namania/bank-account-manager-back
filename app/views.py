32874f2ec63722e652a2c8e7970e32429ccb7e63
Namania <mael.namania@gmail.com>
2025-01-17 15:27:12 +0100

    :construction: authentication system

--- app/views.py
+++ app/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth import authenticate
 
 from .models import Account
 
 def index(request):
+    if "username" not in request.session.keys():
+        return redirect("login")
+    userId = request.session["id"]
     accounts = Account.objects.all()
-    return render(request, "app/index.html", {"accounts": accounts})
+    return render(request, "app/index.html", {"accounts": accounts, "userId": userId})
 
 def accountView(request, accountId):
+    if "username" not in request.session.keys():
+        return redirect("login")
     account = get_object_or_404(Account, pk=accountId)
     return render(request, "app/account.html", {"account": account})
+
+def login(request):
+    error = False
+    if request.method == "POST":
+        user = authenticate(username=request.POST["username"], password=request.POST["password"])
+        if user is not None and not (user.is_staff or user.is_superuser):
+            data = user.__dict__
+            request.session["username"] = data["username"]
+            request.session["password"] = data["password"]
+            request.session["id"] = data["id"]
+            return redirect("index")
+        else:
+            error = True
+    return render(request, "app/login.html", {"error": error})
+
+def logout(request):
+    del request.session["username"]
+    del request.session["password"]
+    return redirect("login")
