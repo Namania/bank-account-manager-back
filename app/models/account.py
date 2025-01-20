e569a3b0db601a23757466f94c1a2b3506b3925c
Namania <mael.namania@gmail.com>
2025-01-20 10:56:42 +0100

    :globe_with_meridians: Owner field in account model

--- app/models/account.py
+++ app/models/account.py
@@ -2,8 +2,10 @@ from django.utils import timezone
 from django.db import models
 from django.contrib import admin
 from djmoney.models.fields import MoneyField
+from django.contrib.auth.models import User
 
 class Account(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     label = models.CharField(max_length=200, verbose_name="Title")
     balance = MoneyField(max_digits=14, decimal_places=2, default_currency='EUR', default=0, verbose_name="Balance")
     create_at = models.DateTimeField(default=timezone.now, verbose_name="Create at")
